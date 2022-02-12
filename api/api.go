5b529ff9a275a0345f34e4b2bdc458934c120a66
nabe4 <takahiro.watanabe.sac@gmail.com>
2022-02-13 03:16:30 +0900

    Slackのトークンをdotenvに移行

--- api/api.go
+++ api/api.go
@@ -1,14 +1,21 @@
 package api
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func PostSlack(text string) {
+	loadEnv()
+	token := os.Getenv("SLACK_TOKEN")
+	fmt.Println(token)
 	params := url.Values{}
-	params.Add("token", "xoxb-2102527046113-3079034186087-tCqKymQmPnA98LxI8z3Ev6Y7")
+	params.Add("token", token)
 	params.Add("channel", "#general")
 	params.Add("text", text)
 	resp, err := http.PostForm("https://slack.com/api/chat.postMessage", params)
@@ -18,4 +25,15 @@ func PostSlack(text string) {
 	}
 	log.Printf("Request Success: %s", resp.Status)
 	// fmt.Println(resp)
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+
+	// message := os.Getenv("SAMPLE_MESSAGE")
+	// fmt.Println(message)
 }
